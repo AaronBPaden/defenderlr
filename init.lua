bacee8ecfeb255a888f50176eeaf309c9873dd52
Aaron Paden <aaronbpaden@gmail.com>
2023-08-04 19:42:00 -0500

    Update init.lua to work on recent MAME versions.
    Remove some outdated documentation. Mame's docs website seems to be more
    complete than it was, so this is no longer needed anyway.

--- init.lua
+++ init.lua
@@ -10,6 +10,10 @@ exports.description = 'Configure left-right controls for Defender (and Stargate)
 exports.license = 'The BSD 3-Clause License'
 exports.author = { name = 'Aaron Paden' }
 
+local reset_subscription = nil
+local stop_subscription = nil
+local frame_subscription = nil
+
 local defenderlr = exports
 function defenderlr.startplugin()
 	-- These two values can be found in the ioport_type enum
@@ -62,7 +66,11 @@ function defenderlr.startplugin()
 		thrust = nil
 		button_left = nil
 		button_right = nil
-		emu.register_frame_done(nil)		
+		if frame_subscription ~= nil then
+			frame_subscription:unsubscribe()
+		end
+		reset_subscription:unsubscribe()
+		stop_subscription:unsubscribe()
 	end
 
 	local function init_plugin()
@@ -72,72 +80,23 @@ function defenderlr.startplugin()
 			else
 				facing_address = 0xA0BB
 			end
-			if tonumber(emu.app_version()) >= 0.227 then
+			if tonumber(emu.app_version()) >= 0.254 then
 				input = manager.machine.input
 				ioport = manager.machine.ioport
 				memory = manager.machine.devices[':maincpu'].spaces['program']
 				thrust = ioport.ports[':IN0'].fields['Thrust']
 				button_left = ioport:type_seq(IPT_JOYSTICK_LEFT, nil, nil)
 				button_right = ioport:type_seq(IPT_JOYSTICK_RIGHT, nil, nil)
-				emu.register_frame_done(process_frame)
+				frame_subscription = emu.add_machine_frame_notifier(process_frame)
 			else
-				print("ERROR: The 'defenderlr' plugin requires MAME version 0.227 or greater.")			
+				print("ERROR: The 'defenderlr' plugin requires MAME version 0.254 or greater.")			
 			end
 		else
 			cleanup()
 		end
 	end
-	-- Unfortunately these are not listed in the documentation on
-	-- docs.mamedev.org, but they're very important for any plugin.
-	-- They take a function as an argument (this is called a callback).
-	-- This function is called on an event. Thankfully there is documentation
-	-- in a comment in luaengine.cpp, which I'll reproduce here:
-	-- emu library
-	-- 
-	-- emu.app_name() - return application name
-	-- emu.app_version() - return application version
-	-- emu.gamename() - return game full name
-	-- emu.romname() - return game ROM name
-	-- emu.softname() - return softlist name
-	-- emu.time() - return emulation time
-	-- emu.pid() - return frontend process ID
-	-- 
-	-- emu.driver_find(driver_name) - find and return game_driver for driver_name
-	-- emu.start(driver_name) - start given driver_name
-	-- emu.pause() - pause emulation
-	-- emu.unpause() - unpause emulation
-	-- emu.step() - advance one frame
-	-- emu.keypost(keys) - post keys to natural keyboard
-	-- emu.wait(len) - wait for len within coroutine
-	-- emu.lang_translate(str) - get translation for str if available
-	-- emu.subst_env(str) - substitute environment variables with values for str (semantics are OS-specific)
-	-- 
-	-- emu.register_prestart(callback) - register callback before reset
-	-- emu.register_start(callback) - register callback after reset
-	-- emu.register_stop(callback) - register callback after stopping
-	-- emu.register_pause(callback) - register callback at pause
-	-- emu.register_resume(callback) - register callback at resume
-	-- emu.register_frame(callback) - register callback at end of frame
-	-- emu.register_frame_done(callback) - register callback after frame is drawn to screen (for overlays)
-	-- emu.register_sound_update(callback) - register callback after sound update has generated new samples
-	-- emu.register_periodic(callback) - register periodic callback while program is running
-	-- emu.register_callback(callback, name) - register callback to be used by MAME via lua_engine::call_plugin()
-	-- emu.register_menu(event_callback, populate_callback, name) - register callbacks for plugin menu
-	-- emu.register_mandatory_file_manager_override(callback) - register callback invoked to override mandatory file manager
-	-- emu.register_before_load_settings(callback) - register callback to be run before settings are loaded
-	-- emu.show_menu(menu_name) - show menu by name and pause the machine
-	-- 
-	-- emu.print_verbose(str) - output to stderr at verbose level
-	-- emu.print_error(str) - output to stderr at error level
-	-- emu.print_info(str) - output to stderr at info level
-	-- emu.print_debug(str) - output to stderr at debug level
-	-- 
-	-- emu.device_enumerator(dev) - get device enumerator starting at arbitrary point in tree
-	-- emu.screen_enumerator(dev) - get screen device enumerator starting at arbitrary point in tree
-	-- emu.image_enumerator(dev) - get image interface enumerator starting at arbitrary point in tree
-	-- emu.image_enumerator(dev) - get image interface enumerator starting at arbitrary point in tree
-	emu.register_start(init_plugin)
-	emu.register_stop(cleanup)
+	reset_subscription = emu.add_machine_reset_notifier(init_plugin)
+	stop_subscription = emu.add_machine_stop_notifier(cleanup)
 end
 
 return exports
